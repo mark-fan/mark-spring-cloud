3cbcf9986abb032b63e7c94bae4d9792876f560d
mark-fan <15289340589@163.com>
2023-01-10 17:30:52 +0800

    spring gateway

---
+++ app1/src/main/java/com/mark/app1/controller/AppController.java
@@ -0,0 +1,24 @@
+package com.mark.app1.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author luofan
+ */
+@RestController("/whichOne")
+public class AppController {
+
+    @Value("${spring.application.name}")
+    private String name;
+
+    @Value("${server.port}")
+    private int port;
+
+    @RequestMapping
+    public String whichOne() {
+        return "我是" + name + " ,端口：" + port;
+    }
+
+}
