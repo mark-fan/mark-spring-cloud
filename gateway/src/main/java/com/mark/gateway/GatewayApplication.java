d6dd2e6760f65abeddf388eb3ea5cc520c8643c6
mark-fan <15289340589@163.com>
2023-01-11 11:21:33 +0800

    自定义路由

--- gateway/src/main/java/com/mark/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/mark/gateway/GatewayApplication.java
@@ -2,7 +2,10 @@ package com.mark.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,4 +15,10 @@ public class GatewayApplication {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p.path("/baidu").filters(f -> f.stripPrefix(1)).uri("https://www.mashibing.com")
+                ).route(p->p.path("/gogo").filters(f -> f.stripPrefix(1)).uri("lb://APP1")).build();
+    }
 }
