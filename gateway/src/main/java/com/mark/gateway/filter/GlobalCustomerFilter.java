869b9672433685dfa2c39486af18291725f90ea6
mark-fan <15289340589@163.com>
2023-01-11 11:25:48 +0800

    自定义过滤器

--- gateway/src/main/java/com/mark/gateway/filter/GlobalCustomerFilter.java
+++ gateway/src/main/java/com/mark/gateway/filter/GlobalCustomerFilter.java
@@ -1,7 +1,31 @@
 package com.mark.gateway.filter;
 
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
 /**
  * @author luofan
  */
-public class GlobalCustomerFilter {
+@Component
+public class GlobalCustomerFilter implements Ordered, GlobalFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        MultiValueMap<String, String> queryParams = request.getQueryParams();
+        queryParams.get("id");
+        System.out.println("hhahhaahha GlobalCustomerFilter");
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
 }
